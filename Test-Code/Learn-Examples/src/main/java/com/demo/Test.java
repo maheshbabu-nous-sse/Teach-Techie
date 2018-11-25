d04550f6fcdf8822ab7420ca930f84bb2a319726
Maheshbabu <mahesh.bandaru656@gmail.com>
2018-11-25 12:15:09 +0530

    Added source code

---
+++ Test-Code/Learn-Examples/src/main/java/com/demo/Test.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Test {
+	
+	
+	public String display(String s) {
+		
+		return s;
+	}
+	
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.display("Hello Mahesh");
+	}
+
+}
